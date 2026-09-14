a7be81b2468e3236ad491e4b6c38389282949a3d
kanchinagababu6-cmd <kanchinagababu6@gmail.com>
2026-09-14 19:49:00 +0530

    Update types.ts

--- clothing-shop/lib/types.ts
+++ clothing-shop/lib/types.ts
@@ -1,13 +1,15 @@
 export interface Product {
   id: string;
   name: string;
-  description: string;
+  description?: string;
   price: number;
   image?: string;
   imageUrl?: string;
   gender?: string;
   category?: string;
-  sizes: string[];
-  colors: string[];
+  sizes?: string[];
+  colors?: string[];
   stock: number;
+  createdAt?: any;
 }
+
